3e28be5c070d895b324130fa4451ff8a048f69ca
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-14 23:21:17 +0530

    posts routes created.

---
+++ src/routes/post.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {protect} from "../middleware/auth.js"
+import {upload } from "../services/cloudinary.js";
+import { createPost, getPosts, getPostById, likeToggle, addComment } from "../controllers/post.controller";
+
+const r = Router();
+
+r.get("/", getPosts);
+r.get("/:id" , getPostById);
+r.post("/", protect, upload.single("media"), createPost);
+r.patch("/:id/like", protect, likeToggle);
+r.post("/:id/comments", protect, addComment);
+
+export default r;
